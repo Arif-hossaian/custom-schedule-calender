cb88c16b1a7742225af9843541bb9ebc66e8abae
Arif-hossaian <arifhossain42136@gmail.com>
2024-06-26 13:17:55 +0600

    add month view

--- src/components/CalenderHeader/MonthHeader.js
+++ src/components/CalenderHeader/MonthHeader.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { addMonths, subMonths, format } from 'date-fns';
-import { getWeekDays } from '../../utils/dateUtlis'
 
-const WeekHeader = ({ currentDate, setCurrentDate }) => {
-  const days = getWeekDays(currentDate);
+const MonthHeader = ({ currentDate, setCurrentDate }) => {
   const goToPreviousMonth = () => setCurrentDate(subMonths(currentDate, 1));
   const goToNextMonth = () => setCurrentDate(addMonths(currentDate, 1));
 
-
-  const firstDay = days[0];
-  const lastDay = days[days.length - 1];
-
-  // Format the dates
-  const formattedFirstDay = format(firstDay, 'MMMM d');
-  const formattedLastDay = format(lastDay, 'MMMM d');
 
   return (
     <div className="flex justify-between p-4 bg-gray-200">
       <button onClick={goToPreviousMonth} className="text-xl">&lt;</button>
       <div className='flex jsutify-center items-center'>
-        <p>{formattedFirstDay} - {formattedLastDay}</p>
-        <p className='font-mono ml-5 underline'>Weekly</p>
+        <p>{format(currentDate, 'MMM yyyy')}</p>
+        <p className='font-mono ml-5 underline'>Month</p>
       </div>
       <button onClick={goToNextMonth} className="text-xl">&gt;</button>
     </div>
   );
 };
 
-export default WeekHeader;
+export default MonthHeader;
--- src/utils/dateUtlis.js
+++ src/utils/dateUtlis.js
@@ -1,4 +1,5 @@
-import { startOfWeek, addDays, addHours, addMinutes, format, parse } from 'date-fns';
+import { startOfWeek, addDays, addHours, addMinutes, format, parse, startOfMonth, endOfMonth, endOfWeek,  eachDayOfInterval } from 'date-fns';
+
 
 export const getWeekDays = (date) => {
   let start = startOfWeek(date);
@@ -9,44 +10,6 @@ export const getWeekDays = (date) => {
   return days;
 };
 
-// export const getDayHours = (date) => {
-//   let start = new Date(date.setHours(9, 0, 0, 0)); // Start from 9:00 AM
-//   let hours = [];
-//   for (let i = 0; i < 8; i++) { // Generate hours from 9:00 AM to 4:00 PM
-//     let hour = addHours(start, i);
-//     hours.push({
-//       time: format(hour, 'HH:mm'),
-//       date: hour.toString(),
-//     });
-//   }
-//   return hours;
-// };
-
-// export const getDayHours = (date) => {
-//   let start = new Date(date);
-//   start.setHours(9, 0, 0, 0); // Start from 9:00 AM
-
-//   const hours = [];
-
-//   for (let i = 0; i < 8; i++) { // Generate hours from 9:00 AM to 4:00 PM
-//     let hour = addHours(start, i);
-
-//     // Push the hour itself
-//     hours.push({
-//       time: format(hour, 'HH:mm'),
-//       date: hour.toString(),
-//       slots: [], // Initialize slots array for each hour
-//     });
-
-//     // Generate 4 slots per hour (15-minute intervals)
-//     for (let j = 0; j < 4; j++) {
-//       const slotHour = addMinutes(hour, j * 15); // Calculate the current slot hour
-//       hours[i].slots.push(format(slotHour, 'HH:mm')); // Push formatted time slot (HH:mm) to the slots array
-//     }
-//   }
-
-//   return hours;
-// };
 
 export const getDayHours = (date) => {
   let start = new Date(date);
@@ -71,6 +34,24 @@ export const getDayHours = (date) => {
     }
   }
 
+  return hours;
+};
+
+
+
+// Get all days in the current month, including days from previous and next month to fill the calendar grid
+export const getMonthViewDays = (currentDate) => {
+  const start = startOfWeek(startOfMonth(currentDate));
+  const end = endOfWeek(endOfMonth(currentDate));
+  return eachDayOfInterval({ start, end });
+};
+
+// Get all hours in a day (you can modify this if you want specific hour ranges)
+export const getDayHoursInMonth = (date) => {
+  let hours = [];
+  for (let i = 0; i < 24; i++) {
+    hours.push(addDays(date, i));
+  }
   return hours;
 };
 
